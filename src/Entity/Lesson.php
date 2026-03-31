bca4f21a3358d2390a0d93e3c768aaf1bebb35f2
Uria <reapuria18@gmail.com>
2026-03-31 17:10:13 +0200

    finished validation system

--- src/Entity/Lesson.php
+++ src/Entity/Lesson.php
@@ -5,8 +5,9 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\LessonRepository;
 
-#[ORM\Entity]
+#[ORM\Entity(repositoryClass: LessonRepository::class)]
 class Lesson
 {
     #[ORM\Id]
--- src/Repository/UserLessonRepository.php
+++ src/Repository/UserLessonRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\User;
 use App\Entity\Theme;
+use App\Entity\Cursus;
 use App\Entity\UserLesson;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -13,6 +14,21 @@ class UserLessonRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, UserLesson::class);
+    }
+
+    public function countValidatedLessonsByCursus(User $user, Cursus $cursus): int
+    {
+        return (int) $this->createQueryBuilder('ul')
+            ->select('COUNT(ul.id)')
+            ->join('ul.lesson', 'l')
+            ->join('l.cursus', 'c')
+            ->where('ul.user = :user')
+            ->andWhere('c = :cursus')
+            ->andWhere('ul.isValidated = true')
+            ->setParameter('user', $user)
+            ->setParameter('cursus', $cursus)
+            ->getQuery()
+            ->getSingleScalarResult();
     }
 
     public function countValidatedLessons(User $user, Theme $theme): int
